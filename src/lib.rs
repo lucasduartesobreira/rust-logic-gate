a826e7a217b10905075ecc08edcbfc53f2b70fe9
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2021-08-28 16:17:46 -0300

    refactor: True and False to a struct containing data instead of a tuple struct type

--- src/lib.rs
+++ src/lib.rs
@@ -3,8 +3,12 @@ trait Boolean {
     fn get_value(&self) -> bool;
 }
 
-pub struct True(bool);
-pub struct False(bool);
+pub struct True {
+    data: bool,
+}
+pub struct False {
+    data: bool,
+}
 
 impl Boolean for True {
     fn boolean<'a>(&'a self, x: &'a dyn Boolean, _: &'a dyn Boolean) -> &'a dyn Boolean {
@@ -12,7 +16,13 @@ impl Boolean for True {
     }
 
     fn get_value(&self) -> bool {
-        self.0
+        self.data
+    }
+}
+
+impl Default for True {
+    fn default() -> Self {
+        True { data: true }
     }
 }
 
@@ -22,23 +32,29 @@ impl Boolean for False {
     }
 
     fn get_value(&self) -> bool {
-        self.0
+        self.data
+    }
+}
+
+impl Default for False {
+    fn default() -> Self {
+        False { data: false }
     }
 }
 
 #[allow(dead_code)]
 fn and<'a>(x: &'a dyn Boolean, y: &'a dyn Boolean) -> &'a dyn Boolean {
-    x.boolean(y, &False(false))
+    x.boolean(y, &False { data: false })
 }
 
 #[allow(dead_code)]
 fn or<'a>(x: &'a dyn Boolean, y: &'a dyn Boolean) -> &'a dyn Boolean {
-    x.boolean(&True(true), y)
+    x.boolean(&True { data: true }, y)
 }
 
 #[allow(dead_code)]
 fn not(x: &dyn Boolean) -> &dyn Boolean {
-    x.boolean(&False(false), &True(true))
+    x.boolean(&False { data: false }, &True { data: true })
 }
 
 #[cfg(test)]
@@ -47,8 +63,8 @@ mod tests {
 
     #[test]
     fn test_and_port() {
-        let t = True(true);
-        let f = False(false);
+        let t = True::default();
+        let f = False::default();
 
         assert!(and(&t, &t).get_value(), "true and true should be true");
         assert!(!and(&f, &t).get_value(), "false and true should be false");
@@ -57,8 +73,8 @@ mod tests {
 
     #[test]
     fn test_or_port() {
-        let t = True(true);
-        let f = False(false);
+        let t = True::default();
+        let f = False::default();
 
         assert!(or(&t, &t).get_value(), "true or true should be true");
         assert!(or(&f, &t).get_value(), "false or true should be true");
@@ -67,8 +83,8 @@ mod tests {
 
     #[test]
     fn test_not_port() {
-        let t = True(true);
-        let f = False(false);
+        let t = True::default();
+        let f = False::default();
 
         assert!(!not(&t).get_value(), "not true should be false");
         assert!(not(&f).get_value(), "not false should be true");
